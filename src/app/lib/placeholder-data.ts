2b321af9b3dfe58cb668f8f7f7baa80392ce6cd9
brenont <brenontavares@gmail.com>
2023-12-22 22:58:26 -0500

    chore: adding post types and post placeholder data

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,32 @@
+import { Post } from "@/app/types/post";
+
+
+export const posts: Post[] = [
+    {
+        postId: '01',
+        coverImg: 'https://www.templeton-recruitment.com/hubfs/c1920_womenintech-299653.jpg',
+        category: 'Technology',
+        title: 'The Impact of Technology on the Workplace: How Technology is Changing',
+        authorName: 'Tracey Wilson',
+        authorImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        createdAt: '2022-12-06'
+    },
+    {
+        postId: '02',
+        coverImg: 'https://www.templeton-recruitment.com/hubfs/c1920_womenintech-299653.jpg',
+        category: 'Technology',
+        title: 'The Impact of Technology on the Workplace: How Technology is Changing',
+        authorName: 'Tracey Wilson',
+        authorImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        createdAt: '2022-12-06'
+    },
+    {
+        postId: '03',
+        coverImg: 'https://www.templeton-recruitment.com/hubfs/c1920_womenintech-299653.jpg',
+        category: 'Technology',
+        title: 'The Impact of Technology on the Workplace: How Technology is Changing',
+        authorName: 'Tracey Wilson',
+        authorImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        createdAt: '2022-12-06'
+    }
+]
---
+++ src/app/types/post.ts
@@ -0,0 +1,9 @@
+export interface Post {
+    postId: string,
+    coverImg: string,
+    category: string,
+    title: string,
+    authorName: string,
+    authorImg: string,
+    createdAt: string
+}
